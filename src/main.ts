f5326baab2fee3f6fc127eec93e7cc3d21acb084
juniorespriella99 <juniorespriella99@gmail.com>
2025-06-16 20:03:08 -0500

    chore(swagger): integración de dependencia de documentación swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Challenge Ally Back')
+    .setDescription('Reto tecnico Ally Back-End Nest.js')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'Bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      in: 'header'
+    }, 'access-token')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {swaggerOptions : {persistAuthorization: true}});
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
