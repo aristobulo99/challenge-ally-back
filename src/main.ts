39f7e7b93610e1fc0311dc707c4890f52da30cb0
juniorespriella99 <juniorespriella99@gmail.com>
2025-06-23 16:39:25 -0500

    feat: ajuste de CORS para consumo de proyectos locales en puerto 4200

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors(
+    {
+      origin: 'http://localhost:4200',
+      credentials: true,
+    }
+  )
+
   const config = new DocumentBuilder()
     .setTitle('Challenge Ally Back')
     .setDescription('Reto tecnico Ally Back-End Nest.js')
